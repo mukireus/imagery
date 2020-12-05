dd669b7c44db1a0272f9308086eeab84cbd3c743
Fatih Aslan <fatihaslan9948@gmail.com>
2020-12-05 13:10:22 +0300

    Added Model

--- lib/ui/home/model/unplash_image.dart
+++ lib/ui/home/model/unplash_image.dart
@@ -1,1 +1,10 @@
-class UnsplashImage {}
+class UnsplashImage {
+  final String id;
+  final String smallUrl;
+  final String fullUrl;
+  final double height;
+  final double width;
+  final String downloadUrl;
+
+  const UnsplashImage({this.id, this.width, this.height, this.smallUrl, this.fullUrl, this.downloadUrl,});
+}
