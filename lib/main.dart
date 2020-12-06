f99a38acc469623c100919a381bbef6bcd58c4cb
Muhammet ÖMER <muhammet.omer@outlook.com>
2020-12-06 16:36:15 +0300

    added theme data

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,18 @@ class Imagery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: buildThemeData,
       title: 'Imagery',
       home: HomeView(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+
+  ThemeData get buildThemeData {
+    return ThemeData(
+      primaryColor: Colors.black,
+      accentColor: Colors.grey[400],
+      indicatorColor: Colors.black,
     );
   }
 }
--- lib/ui/components/loading_indicator.dart
+++ lib/ui/components/loading_indicator.dart
@@ -4,7 +4,10 @@ class LoadingIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(),
+      child: CircularProgressIndicator(
+        semanticsLabel: "Yükleniyor...",
+        semanticsValue: "Yükleniyor...",
+      ),
     );
   }
 }
